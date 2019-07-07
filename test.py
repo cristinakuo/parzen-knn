19718980e3c332c408420c9be2642776b69d6cde
Cristina <ckuo@fi.uba.ar>
2019-07-07 12:58:18 -0300

    Remove test from repo

--- test.py
+++ test.py
@@ -1,17 +1,19 @@
+# d) Realizar un clasificador para B y C y clasificar 10e2 muestras nuevas
 import numpy as np
 import matplotlib.pyplot as plt
-if __name__ == '__main__':
-    N = 100
-    x_mix = []
+import parzen
+import knn
+import plots
 
-    U = np.random.uniform(0, 1, N)
-    for i in range(0,N):
-        if U[i] > 0.5:
-            sample = np.random.uniform(2,10)
-        else:
-            sample = np.random.normal(2,4)
 
-        x_mix.append(sample)
-    # Comprobamos que sale bien
-    plt.hist(x_mix, bins='auto')
-    plt.show()
+N = int(10e4)
+CLASS_1 = 1
+CLASS_2 = 2
+x_samples_1 = np.random.uniform(2,10,N)
+x_samples_2 = np.random.normal(2,4,N)
+
+
+# e) Implementar clasificacion del K vecino mas cercano para K = 1, 11 y 51.
+
+#       Calcular el error al clasificar las mismas muestras de D).
+# Conclusionesss
