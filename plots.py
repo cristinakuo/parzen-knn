fef50a1409bde7d9de10f894f57bd8026c546ace
Cristina <ckuo@fi.uba.ar>
2019-07-07 20:28:29 -0300

    Add: Plotting details

--- plots.py
+++ plots.py
@@ -15,15 +15,14 @@ def plot_with_labels(x,label_real,label_test):
 
 
 def plot_distributions(x,px_1,px_2):
-    plt.plot(x,px_1,'.r')
-    plt.plot(x,px_2,'.b')
+    plt.plot(x,px_1,'.',markersize=3,label='Distribucion 1')
+    plt.plot(x,px_2,'.',markersize=3,label='Distribucion 2')
+    plt.legend()
     plt.show()
 
+if __name__ == '__main__':
+    p1 = [1,2,3,4,5]
+    p2 = [6,7,8,9,10]
+    x = [1,2,3,4,5]
+    plot_distributions(x,p1,p2)
 
-# Error
-# TODO: sacar de aca
-def get_error(label_real,label_test):
-    label_error = [a - b for a, b in zip(label_real, label_test)]
-    n_errors = len([n for n in label_error if n != 0])
-    error_rate = n_errors / len(label_error)
-    return error_rate
