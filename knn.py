c38250232c415d0521472f7f7ed1242132b10f84
Cristina <ckuo@fi.uba.ar>
2019-07-08 12:15:16 -0300

    Fix knn_estimate window

--- knn.py
+++ knn.py
@@ -4,13 +4,15 @@ import matplotlib.pyplot as plt
 CLASS_1 = 1
 CLASS_2 = 2
 
-# Devuelve los k vecinos mas cercanos en la lista 'data' del valor 'ref'.
-# Devuelve una lista de k tuplas: (indice,valor)
+# Returns k nearest neighbors to the value 'ref' found in 'data'.
+# Return value type is a list of k tuples: (index,value)
 def knn_search(k,data,ref):
+    # Add index to data
     data_indexed = enumerate(data)
+    # Sort by smaller distance to ref and keep the first k tuples
     return sorted(data_indexed, key=lambda x: abs(x[1]-ref))[:k]
 
-# Devuelve la probabilidad estimada con knn evaluada en los puntos de X
+# Returns knn estimated density evaluated in X
 def knn_estimate(k,samples_data,X):
     p_estimate = []
     N = len(samples_data)
@@ -18,13 +20,15 @@ def knn_estimate(k,samples_data,X):
     if k == 1:
         for x in X:
             k_nearest = knn_search(k, samples_data, x)
-            nearest = [x[1] for x in k_nearest]  # TODO: es solo 1 tuple
+            nearest = [x[1] for x in k_nearest] # get the value
             p_estimate.append(1/(2*abs(x-nearest[0])))
     else:
         for x in X:
             k_nearest = knn_search(k,samples_data,x)
-            k_nearest = [x[1] for x in k_nearest] # keeps second element of each tupple
-            V = abs(max(k_nearest)-min(k_nearest))
+            k_nearest = [x[1] for x in k_nearest] # get the second element (value) of each tuple
+            max_val = max(k_nearest+[x])
+            min_val = min(k_nearest+[x])
+            V = abs(max_val-min_val)
             p_estimate.append(k/(N*V))
 
     return p_estimate
@@ -70,13 +74,14 @@ if __name__ == "__main__":
     N = int(10e4)
     x_samples_1 = np.random.uniform(2, 10, N).tolist() # arrays
     x_samples_2 = np.random.normal(2, 4, N).tolist()
-    X = np.linspace(-10, 20, 100)
-    #p_estimate = knn_estimate(k,x_samples_normal, X)
+    X = np.linspace(-10, 20, 1000)
+    k = 2
+    p_estimate = knn_estimate(k,x_samples_2, X)
 
-    #plt.plot(X, p_estimate, 'r-')
-    #plt.show()
+    plt.plot(X, p_estimate, 'r-')
+    plt.show()
 
-    a = [1,2,3,4,5,6,7,8,-10]
-    test_label = knn_classify(5,x_samples_1,x_samples_2,a)
-    print(test_label)
+    #a = [1,2,3,4,5,6,7,8,-10]
+    #test_label = knn_classify(5,x_samples_1,x_samples_2,a)
+    #print(test_label)
 
